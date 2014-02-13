43ef696fa95111618be4a54589829e2fdefc6a25
SourenP <souren@papazian.info>
2014-02-13 12:03:11 -0500

    changed submenu to list that opens

--- app.js
+++ app.js
@@ -3,38 +3,23 @@ function on_load()
 	$('.mainMenuItem').click(function()
 	{
 		var currMMI = $(this)
-		var	currSMI =  $(get_submenuItem_class(currMMI))
+		var	currSML =  $(get_submenuList_class(currMMI))
 
 		if(currMMI.hasClass("clicked"))
 		{
 			currMMI.removeClass("clicked");
-
-			$(currSMI.get().reverse()).each(function(index)
-			{
-				var currSubEl = $(this);
-
-		        setTimeout(function() {
-		            currSubEl.slideUp(75)
-		        }, 75 * index);
-			});
+			currSML.slideUp(300);
 		}	
 		else
 		{
 			currMMI.addClass("clicked");
-
-			currSMI.each(function(index)
-			{
-				var currSubEl = $(this);
-		        setTimeout(function() {
-		            currSubEl.slideDown(75);
-		        }, 75 * index);
-			});		
+			currSML.slideDown(300);
 		}
 
 	});
 }
 
-function get_submenuItem_class(mainMenuItemClass)
+function get_submenuList_class(mainMenuItemClass)
 {
 	return ('.' + 'sub' + $(mainMenuItemClass).attr("class").split(/\s+/)[1]);
 }
