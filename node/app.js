4fb9e5c555ea6b61203e47097a184a9e3492117b
Zachary Bush <Zachary.bush@meraki.net>
2014-03-22 22:26:26 -0700

    Start making the express app

---
+++ node/app.js
@@ -0,0 +1,37 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var React = require('react/addons');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(function(req, res, next) {
+  if (req.url.indexOf("/api") === -1) {
+    res.send(React.renderComponentToString(React.DOM.div()));
+  } else {
+    next();
+  }
+});
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
